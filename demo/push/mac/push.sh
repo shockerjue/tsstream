e0bbf2295559d915366ad47f4cacbd095243a483
shockerjue <shockerjue@126.com>
2019-03-08 17:02:34 +0800

    fix: 修改mac下推流的东西

--- demo/push/mac/push.sh
+++ demo/push/mac/push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-while true
-do
-    ./ffmpeg -threads 8 -rtbufsize 16M  -framerate 30 -video_size 1280x720  -f avfoundation -i "1" -f mpegts  -codec:v mpeg1video -s 1280x720 -b:v 256k -bufsize 1300k -bufsize 2800k -maxrate 1500k -bf 0 -r 30 -muxdelay 0.001  tcp://47.75.129.127:50001
+# while true
+# do
+    ./ffmpeg -threads 8 -rtbufsize 16M  -framerate 20 -pixel_format yuyv422 -video_size  640x480  -f avfoundation -i "1" -f mpegts  -codec:v mpeg1video -s 640x480 -b:v 128k -bufsize 128k -bufsize 512k -maxrate 1024k -bf 0 -r 20 -muxdelay 0.001  tcp://120.78.156.129:50001
     ping -c 3 127.0.0.1 > /dev/null
-done
+# done
 
 exit

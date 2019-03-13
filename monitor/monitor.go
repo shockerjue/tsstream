a28eb1369297b39aba40737533d24b5a20537f4c
shockerjue <shockerjue@126.com>
2019-03-13 13:24:27 +0800

    fix: add cross

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -138,7 +138,7 @@ func Cross() gin.HandlerFunc {
 			fmt.Println("origin-->", origin)
 			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 			c.Header("Access-Control-Allow-Origin", origin)                                     // 这是允许访问所有域
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
+			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
 			//  header的类型
 			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
 			//              允许跨域设置                                                                                                      可以返回其他子段
@@ -172,6 +172,7 @@ func (this *MonitorServer)RunServer() (err error) {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	r.Use(Cross())
+
 	authorized := r.Group("/")
 	authorized.Use(func(c *gin.Context) {	
 	})
